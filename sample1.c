94372f607cb0d557f7474aea141a20c2cd9781da
ANARGH-PREM <ttc0822012@nttf.co.in>
2024-08-30 14:41:10 +0530

    test1

---
+++ sample1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int length,breadth,area=0;
+	printf("enter the length");
+	scanf("%d",&length);
+	print("enter the breadth");
+	scanf("%d",&breadth);
+	area=length*breadth;
+	printf("area is %d",area);
+}
